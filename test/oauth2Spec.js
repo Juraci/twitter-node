d7efaa25b966fcaef1ba7abe46408e1c9d304466
Juraci Vieira <juraci.vieira@gmail.com>
2016-02-18 23:39:32 -0200

    Using sinon-chai for syntax sugar

--- test/oauth2Spec.js
+++ test/oauth2Spec.js
@@ -1,6 +1,11 @@
-var expect = require('chai').expect;
+"use strict";
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+var expect = chai.expect;
+chai.use(sinonChai);
 
-describe('OAUTH2 authentication', function() {
+describe('OAUTH2 authentication module', function() {
     var OAUTH2;
 
     beforeEach(function() {
@@ -26,5 +31,26 @@ describe('OAUTH2 authentication', function() {
         expect(function () {
             OAUTH2(args);
         }).to.throw(/consumerSecret not provided/);
+    });
+
+    describe('#getBearerToken', function() {
+        it('should propagate the bearer token to the callback function', function() {
+            var request = {
+                post: function(opts, callback) {
+                    return callback(null, {}, JSON.stringify({access_token: 'fake_token'}));
+                }
+            };
+
+            var args = {
+                consumerKey: 'foo',
+                consumerSecret: 'bar',
+                req: request
+            };
+
+            var callback = sinon.spy();
+
+            OAUTH2(args).getBearerToken(callback);
+            expect(callback).to.have.been.calledWith(null, 'fake_token', request);
+        });
     });
 });
